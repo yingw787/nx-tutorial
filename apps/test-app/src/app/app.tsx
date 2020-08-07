92b05bffe042bf8364fe5054c87bea5163a8b3f9
Ying Wang <yingw787.personal@gmail.com>
2020-08-07 10:29:03 -0400

    Update app.tsx adding ability to add a todo.

--- apps/test-app/src/app/app.tsx
+++ apps/test-app/src/app/app.tsx
@@ -10,6 +10,15 @@ export const App = () => {
     { title: 'Todo 2' },
   ]);
 
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        title: `New todo ${Math.floor(Math.random() * 1000)}`
+      },
+    ]);
+  }
+
   return (
     <>
       <h1>Todos</h1>
@@ -18,6 +27,9 @@ export const App = () => {
           <li className={'todo'}>{t.title}</li>
         ))}
       </ul>
+      <button id={`add-todo`} onClick={addTodo}>
+        Add Todo
+      </button>
     </>
   )
 }
